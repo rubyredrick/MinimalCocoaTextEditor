13119401c00de4fe3d48cffc946d5a292181d386
Rick DeNatale <rick@gmail.com>
2018-04-30 09:11:20 -0400

    Initial Commit

---
+++ MinimalCocoaTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MinimalCocoaTextEditor
+//
+//  Created by Rick DeNatale on 4/30/18.
+//  Copyright © 2018 denhaven2. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MinimalCocoaTextEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MinimalCocoaTextEditor
+//
+//  Created by Rick DeNatale on 4/30/18.
+//  Copyright © 2018 denhaven2. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
